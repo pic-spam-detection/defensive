10e23bd1e4461c21996b281edd89a0a7915b3e68
Corentin LACHEVRE <corentin.lachevre@ensta-paris.fr>
2025-02-04 17:22:22 +0100

    ajout support BERT

--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -9,32 +9,53 @@ from models.vectorizer import Vectorizer
 
 class NaiveBayes:
     
-    def __init__(self, dataset, checkpoint_path=None):
+    def __init__(self, dataset, type='sklearn', checkpoint_path=None):
         self.classifier = MultinomialNB()
-        self.vectorizer = Vectorizer(checkpoint_path=checkpoint_path).vectorizer
+        self.vectorizer = Vectorizer(type=type, checkpoint_path=checkpoint_path).vectorizer
+        self.type = type
 
-        if checkpoint_path is not None:
-            self.classifier = joblib.load(checkpoint_path + 'classifier.joblib')
-        
     def train(self, dataset, save_path=None):
 
         mails_df = pd.DataFrame(dataset)
         mails_df['text'] = mails_df['subject'] + ' ' + mails_df['body']
 
+
         y = mails_df['ground_truth']
 
         X_train, X_test, y_train, y_test = train_test_split(mails_df['text'], y, test_size=0.2, random_state=47)
 
-        X_train = self.vectorizer.fit_transform(X_train)
-        X_test = self.vectorizer.transform(X_test)
-        self.classifier.fit(X_train, y_train)
+        if self.type == 'bert':
+            X_train = X_train.to_list()
+            X_test = X_test.to_list()
 
-        if save_path is not None:
-            joblib.dump(self.classifier, save_path + 'classifier.joblib')
-            joblib.dump(self.vectorizer, save_path + 'vectorizer.joblib')
+            inputs_train = self.vectorizer(X_train, return_tensors='np', truncation=True, max_length=10000, padding="max_length")
+            inputs_test = self.vectorizer(X_test, return_tensors='np', truncation=True, max_length=10000, padding="max_length")
 
-        y_pred = self.classifier.predict(X_test)
+            X_train_ids = inputs_train['input_ids']
+            X_test_ids = inputs_test['input_ids']
 
+            # Flatten the arrays to 2D
+            X_train_ids = X_train_ids.reshape(X_train_ids.shape[0], -1)
+            X_test_ids = X_test_ids.reshape(X_test_ids.shape[0], -1)
+
+
+            self.classifier.fit(X_train_ids, y_train)
+            y_pred = self.classifier.predict(X_test_ids)
+
+        elif self.type == 'sklearn':
+
+            X_train = self.vectorizer.fit_transform(X_train)
+            X_test = self.vectorizer.transform(X_test)
+
+            self.classifier.fit(X_train, y_train)
+
+
+            if save_path is not None:
+                joblib.dump(self.classifier, save_path + 'classifier_bayes.joblib')
+                joblib.dump(self.vectorizer, save_path + 'vectorizer_sklearn.joblib')
+
+            y_pred = self.classifier.predict(X_test)
+            
         accuracy = (y_pred == y_test).mean()
         print(f"Accuracy: {accuracy} {accuracy_score(y_test, y_pred)}")
         return accuracy
@@ -43,24 +64,28 @@ class NaiveBayes:
         """
         args:
             mail: dict
-                mail information (adress, domain, domain_extension, subject, body, ground_truth)
+                mail information (address, domain, domain_extension, subject, body, ground_truth)
 
         returns:
             (boolean) prediction 0 if ham, 1 if spam, (boolean) prediction == ground_truth
         """
         try:
-            adress = mail['adress']
+            adress = mail['address']
             domain = mail['domain']
             domain_extension = mail['domain_extension']
             subject = mail['subject']
             body = mail['body']
             ground_truth = mail['ground_truth']
         except:
-            raise ValueError("mail should be dictionary like with 'adress', 'domain', 'domain_extension', 'subject', 'body' and 'ground_truth'")
+            raise ValueError("Mail should be dictionary like 'address', 'domain', 'domain_extension', 'subject', 'body' and 'ground_truth'")
 
 
         text =  mail['subject'] + ' ' + mail['body']
-        X = self.vectorizer.transform(text)
+
+        if self.type == 'bert':
+            X = self.vectorizer(text.to_list(), return_tensors='np', truncation=True, max_length=10000, padding="max_length")['input_ids']
+        elif self.type == 'sklearn':
+            X = self.vectorizer.transform(text)
         pred = self.classifier.predict(X)
 
         return pred, pred==ground_truth
--- models/vectorizer.py
+++ models/vectorizer.py
@@ -1,14 +1,25 @@
 
 from sklearn.feature_extraction.text import CountVectorizer
+from transformers import BertTokenizer
 import joblib
-
+import torch
 
 class Vectorizer:
 
-    def __init__(self, checkpoint_path=None):
-        if checkpoint_path is not None:
-            self.classifier = joblib.load(checkpoint_path + 'classifier.joblib')
-            self.vectorizer = joblib.load(checkpoint_path + 'vectorizer.joblib')
-        else:
-            self.vectorizer = CountVectorizer(stop_words="english")
+    def __init__(self, type='sklearn', checkpoint_path=None):
 
+        if type == 'sklearn':
+            self.sklearn(checkpoint_path)
+
+        elif type == 'bert':
+            self.bert()
+
+    def sklearn(self, checkpoint_path=None):
+        if checkpoint_path is not None:
+            self.vectorizer = joblib.load(checkpoint_path + 'vectorizer_sklearn.joblib')
+        else:
+            self.vectorizer = CountVectorizer()
+
+    def bert(self):
+        # Charger le tokenizer BERT
+        self.vectorizer = BertTokenizer.from_pretrained('bert-base-uncased')
