d617ffdfd5c8d974b6ac8b959bdbe21c51f17204
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-03-12 13:48:38 +0100

    Add typing

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -1,8 +1,15 @@
+from typing import Any, List
+
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
 
-def get_dataloader(embeddings, labels, batch_size=32, shuffle=False):
+def get_dataloader(
+    embeddings: Any,
+    labels: List[str],
+    batch_size: int = 32,
+    shuffle: bool = False,
+) -> DataLoader:
     embeddings_tensor = embeddings.clone().detach().float()
     labels_tensor = torch.tensor(labels, dtype=torch.long)
 
