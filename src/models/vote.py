4c10e098b8b5985f97c2a549d3f313b856082299
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-03-13 22:57:09 +0100

    Add usage of checkpoints

--- src/models/vote.py
+++ src/models/vote.py
@@ -5,16 +5,26 @@ import pandas as pd
 import numpy as np
 import torch
 
-MODELS = ["naive_bayes", "logistic_regression", "svm"]
-# MODELS = ["naive_bayes", "logistic_regression",]
-
-
+from src.utils.const import CLASSICAL_ML_MODELS
 
 
 class Vote(BaseModel):
-    def __init__(self, models_type: List[BaseModel] = MODELS, checkpoint_path: Optional[str] = None, threshold: float = 0.5):
+    def __init__(
+        self,
+        models_type: List[BaseModel] = CLASSICAL_ML_MODELS,
+        checkpoint_paths: Optional[List[str]] = None,
+        threshold: float = 0.5,
+    ):
         super().__init__()
-        self.models = [ClassicalMLClassifier(classifier_type=model, checkpoint_path=checkpoint_path) for model in models_type]
+        self.models = [
+            ClassicalMLClassifier(
+                classifier_type=model,
+                checkpoint_path=(
+                    checkpoint_paths[index] if checkpoint_paths is not None else None
+                ),
+            )
+            for index, model in enumerate(models_type)
+        ]
         self.threshold = threshold
 
     def vote(self, mail: List[str], models: List[BaseModel]):
@@ -27,33 +37,34 @@ class Vote(BaseModel):
         """
         if models is None:
             return -1
-        results = [model.classify(mail).reshape(1,-1)[0] for model in models]
+        results = [model.classify(mail).reshape(1, -1)[0] for model in models]
         spam_votes = np.sum(results)
-        return 1 if spam_votes/len(models) > self.threshold else 0
-    
+        return 1 if spam_votes / len(models) > self.threshold else 0
+
     def votes(self, mail, models: List[BaseModel]):
         """
         Main function used to detect spam mails
 
         :param tensor mail: tensor containing the mails already vectorized
         :param model: model used to detect spam mails
-        :return: 
+        :return:
         """
         if models is None:
             return -1
         results = [model.predict(mail) for model in models]
         spam_votes = np.sum(results, axis=0)
-        return torch.Tensor(spam_votes/len(models) > self.threshold).int()  
-    
+        return torch.Tensor(spam_votes / len(models) > self.threshold).int()
+
     def classify(self, mail: List[str]) -> int:
         return self.vote(mail, self.models)
-    
-    
+
     def predict(self, X: Union[List[str], pd.Series]) -> np.ndarray:
         """Predict labels for a list of texts"""
         return self.votes(X, self.models)
-    
-    def train(self, dataset: List[Dict[str, str]], save_path: Optional[str] = None) -> Dict[str, BaseModel]:
+
+    def train(
+        self, dataset: List[Dict[str, str]], save_path: Optional[str] = None
+    ) -> Dict[str, BaseModel]:
         """Train the model on the provided dataset"""
         for model in self.models:
             model.train(dataset, save_path)
